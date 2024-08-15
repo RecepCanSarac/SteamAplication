64a729cdf65bc10161be27915421c68d5cc33ca8
Recep Can Saraç <113214739+RecepCanSarac@users.noreply.github.com>
2024-08-16 01:47:17 +0300

    Class Multiplayer change

--- SteamAplication/Assets/Scripts/UI/ClassGenerator.cs
+++ SteamAplication/Assets/Scripts/UI/ClassGenerator.cs
@@ -1,35 +1,44 @@
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using Mirror;
 
 public enum ClassType
 {
     Doctor, Detective, Seer, Armored, Confessor, Thief, Trapper, Buffoon, Lookout, Killer
 }
 
-public class ClassGenerator : MonoBehaviour
+public class ClassGenerator : NetworkBehaviour
 {
     public Transform Content;
-    public GameObject classItem;
+    public GameObject classItemPrefab;
     public List<SOClass> classes = new List<SOClass>();
     public List<SOClass> Userclasses = new List<SOClass>();
     private Dictionary<ClassType, int> classStackCounts = new Dictionary<ClassType, int>();
     private Dictionary<ClassType, GameObject> spawnedClassItems = new Dictionary<ClassType, GameObject>();
 
-    public GameObject Class;
+    public GameObject ClassPrefab;
     public Transform ListContent;
-    private int index = 0;
 
     private void Start()
     {
         for (int i = 0; i < classes.Count; i++)
         {
-            GameObject itemIns = Instantiate(classItem, Content);
-            itemIns.GetComponent<ClassItem>().Setup(classes[i].ClassName, classes[i].ClassIcon, classes[i].ClassType);
-            itemIns.GetComponent<ClassItem>().userClass = classes[i];
+            CreateClassItem(classes[i]);
         }
     }
 
+    [Server]
+    private void CreateClassItem(SOClass classData)
+    {
+        GameObject itemIns = Instantiate(classItemPrefab, Content);
+        var classItem = itemIns.GetComponent<ClassItem>();
+        classItem.Setup(classData.ClassName, classData.ClassIcon, classData.ClassType);
+        classItem.userClass = classData;
+        NetworkServer.Spawn(itemIns);
+    }
+
+    [Server]
     public void UpdateStackedClasses(SOClass newClass)
     {
         ClassType classType = newClass.ClassType;
@@ -37,27 +46,36 @@ public class ClassGenerator : MonoBehaviour
         if (!classStackCounts.ContainsKey(classType))
         {
             classStackCounts[classType] = 0;
-            GameObject classIns = Instantiate(Class, ListContent);
-            classIns.GetComponent<ClassItem>().Setup(newClass.ClassName, newClass.ClassIcon, newClass.ClassType);
+
+            GameObject classIns = Instantiate(ClassPrefab, ListContent);
+            var classItem = classIns.GetComponent<ClassItem>();
+            classItem.Setup(newClass.ClassName, newClass.ClassIcon, newClass.ClassType);
+            NetworkServer.Spawn(classIns); 
             spawnedClassItems[classType] = classIns;
         }
-
-        classStackCounts[classType]++;
-        UpdateStackCount(classType);
+        else
+        {
+            classStackCounts[classType]++;
+            UpdateStackCount(classType);
+        }
     }
 
     private void UpdateStackCount(ClassType classType)
     {
         if (classStackCounts.ContainsKey(classType))
         {
-            TextMeshProUGUI text = spawnedClassItems[classType].GetComponent<ClassItem>().NumberText;
-            if (classStackCounts[classType] > 1)
+            var classItem = spawnedClassItems[classType];
+            if (classItem != null)
             {
-                text.text = classStackCounts[classType].ToString();
-            }
-            else
-            {
-                text.text = "1";
+                TextMeshProUGUI text = classItem.GetComponent<ClassItem>().NumberText;
+                if (classStackCounts[classType] > 1)
+                {
+                    text.text = classStackCounts[classType].ToString();
+                }
+                else
+                {
+                    text.text = "1";
+                }
             }
         }
     }
--- SteamAplication/Assets/Scripts/UI/ClassItem.cs
+++ SteamAplication/Assets/Scripts/UI/ClassItem.cs
@@ -1,11 +1,11 @@
+using Mirror;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class ClassItem : MonoBehaviour
+public class ClassItem : NetworkBehaviour
 {
     public SOClass userClass;
-
     public Image icon;
     public TextMeshProUGUI ClassName;
     public ClassType type;
@@ -13,10 +13,17 @@ public class ClassItem : MonoBehaviour
     public Button SetClassButton;
 
     private ClassGenerator ClassGenerator;
-    public void Setup(string _name, Sprite _icon, ClassType _type)
+
+    [SyncVar(hook = nameof(OnStackCountChanged))]
+    private int stackCount;
+
+    private void Awake()
     {
         ClassGenerator = GameObject.Find("ClassListUI").GetComponent<ClassGenerator>();
+    }
 
+    public void Setup(string _name, Sprite _icon, ClassType _type)
+    {
         ClassName.text = _name;
         icon.sprite = _icon;
         type = _type;
@@ -30,10 +37,17 @@ public class ClassItem : MonoBehaviour
         }
     }
 
-
     public void FromOldListNewList(ClassGenerator _ClassGenerator)
     {
-        _ClassGenerator.Userclasses.Add(this.userClass);
-        _ClassGenerator.UpdateStackedClasses(this.userClass);
+        if (isServer)
+        {
+            _ClassGenerator.Userclasses.Add(this.userClass);
+            _ClassGenerator.UpdateStackedClasses(this.userClass);
+        }
+    }
+
+    private void OnStackCountChanged(int oldCount, int newCount)
+    {
+        NumberText.text = newCount > 1 ? newCount.ToString() : "1";
     }
 }
