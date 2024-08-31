ff0a0b827d79bcae92005d84fe593ac1edd9ba3e
Recep Can Saraç <113214739+RecepCanSarac@users.noreply.github.com>
2024-08-31 13:55:07 +0300

    Classs texts fixed

--- SteamAplication/Assets/Scripts/ClassSystem/PlayerClass.cs
+++ SteamAplication/Assets/Scripts/ClassSystem/PlayerClass.cs
@@ -8,19 +8,22 @@ public class PlayerClass : NetworkBehaviour
 
     public List<string> className = new List<string>();
 
-    private int indeks = 0;
 
     public void ShowClasses()
     {
-        if (indeks > texts.Length) return;
-        texts[indeks].text = className[indeks];
-        indeks++;
+        //Change Recep
+        for (int i = 0; i < className.Count; i++)
+        {
+            texts[i].gameObject.SetActive(true);
+            texts[i].text = className[i];
+        }
     }
     //Add recep
     public void ShowClasses(List<string> clases)
     {
         for (int i = 0; i < texts.Length; i++)
         {
+            texts[i].gameObject.SetActive(true);
             texts[i].text = clases[i];
         }
     }
