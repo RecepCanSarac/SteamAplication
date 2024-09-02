3b5f2b548e5a9ba6687305503e808ca757f3c75e
Recep Can Saraç <113214739+RecepCanSarac@users.noreply.github.com>
2024-09-02 23:35:28 +0300

    Update

--- SteamAplication/Assets/Scripts/ClassSystem/PlayerClass.cs
+++ SteamAplication/Assets/Scripts/ClassSystem/PlayerClass.cs
@@ -1,11 +1,12 @@
 using System.Collections.Generic;
 using Mirror;
 using TMPro;
+using UnityEngine;
 
 public class PlayerClass : NetworkBehaviour
 {
     public TextMeshProUGUI[] texts;
-
+    public GameObject[] ClassItems;
     public List<string> className = new List<string>();
 
     private void Start()
@@ -19,6 +20,7 @@ public class PlayerClass : NetworkBehaviour
         //Ben yaptım tabii güzel
         for (int i = 0; i < className.Count; i++)
         {
+            ClassItems[i].SetActive(true);
             texts[i].gameObject.SetActive(true);
             texts[i].text = className[i];
         }
@@ -28,6 +30,7 @@ public class PlayerClass : NetworkBehaviour
     {
         for (int i = 0; i < texts.Length; i++)
         {
+            ClassItems[i].SetActive(true);
             texts[i].gameObject.SetActive(true);
             texts[i].text = clases[i];
         }
