1812cf7e2b60bf17fd0ee9f0a2f083f9d9544d6f
Recep Can Saraç <113214739+RecepCanSarac@users.noreply.github.com>
2024-09-04 00:21:36 +0300

    Update TVScript.cs

--- SteamAplication/Assets/Scripts/UI/TVScript.cs
+++ SteamAplication/Assets/Scripts/UI/TVScript.cs
@@ -30,19 +30,31 @@ public class TVScript : NetworkBehaviour
         {
             playerClass = Manager.GamePlayers[0].gameObject;
         }
-        for (int i = 0; i < playerClass.GetComponent<PlayerClass>().playerClasses.Count; i++)
+
+        var playerClassComponent = playerClass.GetComponent<PlayerClass>();
+
+        if (playerClassComponent.playerClasses.Count == 0)
         {
-            if (playerClass.GetComponent<PlayerClass>().playerClasses.Count > 0)
+            foreach (var item in items)
             {
-                names[i].text = playerClass.GetComponent<PlayerClass>().playerClasses[i].ClassName;
-                numbers[i].text = playerClass.GetComponent<PlayerClass>().playerClasses[i].Count.ToString();
+                item.gameObject.SetActive(false);
+            }
+            return;
+        }
+
+        for (int i = 0; i < items.Length; i++)
+        {
+            if (i >= playerClassComponent.playerClasses.Count || playerClassComponent.playerClasses[i].Count == 0)
+            {
+                items[i].gameObject.SetActive(false);
             }
             else
             {
-
-                numbers[i].text = 0.ToString();
+                items[i].gameObject.SetActive(true);
+                names[i].text = playerClassComponent.playerClasses[i].ClassName;
+                numbers[i].text = playerClassComponent.playerClasses[i].Count.ToString();
             }
         }
-
     }
+
 }
