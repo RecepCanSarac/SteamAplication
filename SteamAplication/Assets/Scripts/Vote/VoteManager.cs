33bdbff82c7833f58c13617f67dd7d0a838315c4
Yusuf <yusuf_zeki1234@hotmail.com>
2024-09-16 18:31:19 +0300

    Update VoteManager.cs

--- SteamAplication/Assets/Scripts/Vote/VoteManager.cs
+++ SteamAplication/Assets/Scripts/Vote/VoteManager.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class VoteManager : MonoBehaviour
+public class VoteManager : NetworkBehaviour
 {
     public static VoteManager Instance;
 
