fd418873b22be42959ee994ab035877e1c13bf0a
Yusuf <yusuf_zeki1234@hotmail.com>
2024-09-25 11:12:19 +0300

    day time added

--- SteamAplication/Assets/Scripts/Game/DayTime.cs
+++ SteamAplication/Assets/Scripts/Game/DayTime.cs
@@ -1,45 +1,79 @@
+using System.Collections;
 using UnityEngine;
 using RenderSettings = UnityEngine.RenderSettings;
 
-public class DayTime : MonoBehaviour, IDayTimeCycle
+public class DayTime : MonoBehaviour
 {
+    [Header("Skybox Materials")]
+    [Space]
     public Material morningMaterial;
     public Material nightMaterial;
+    [Space]
 
+    [Header("Sun Settings")]
+    [Space]
     public Light sun;
+    public float transitionSpeed = 1.0f;
+    [Space]
 
-    public bool isChanged = false;
+    [Header("Sun Rotation")]
+    [Space]
+    public Vector3 dayRotation = new Vector3(50, 0, 0);
+    public Vector3 nightRotation = new Vector3(-50, 0, 0);
+
+    private bool isNight = false;
+    private bool isTransitioning = false;
 
     void Update()
     {
-        isChanged = Input.GetKey(KeyCode.Space);
-
-        if (isChanged == false)
-        {
-            SetDayTime();
-        }
-        else
-        {
-            SetNightTime();
-        }
+        HandleInput();
     }
 
-    public void SetDayTime()
+    void HandleInput()
     {
-        RenderSettings.skybox = morningMaterial;
-        sun.intensity = 1;
+        if (Input.GetKey(KeyCode.Space) && !isNight && !isTransitioning)
+        {
+            StartTransitionToNight();
+        }
+        else if (!Input.GetKey(KeyCode.Space) && isNight && !isTransitioning)
+        {
+            StartTransitionToDay();
+        }
     }
 
-    public void SetNightTime()
+    void StartTransitionToNight()
     {
-        RenderSettings.skybox = nightMaterial;
-        sun.intensity = 0;
+        StartCoroutine(TransitionTime(nightMaterial, 0f, nightRotation, () => isNight = true));
     }
-}
 
-public interface IDayTimeCycle
-{
-    void SetDayTime();
+    void StartTransitionToDay()
+    {
+        StartCoroutine(TransitionTime(morningMaterial, 1f, dayRotation, () => isNight = false));
+    }
 
-    void SetNightTime();
+    IEnumerator TransitionTime(Material targetSkybox, float targetIntensity, Vector3 targetRotation, System.Action onComplete)
+    {
+        isTransitioning = true;
+
+        float initialIntensity = sun.intensity;
+        Vector3 initialRotation = sun.transform.eulerAngles;
+
+        float elapsedTime = 0f;
+        float duration = Mathf.Abs(targetIntensity - initialIntensity) / transitionSpeed;
+
+        while (elapsedTime < duration)
+        {
+            elapsedTime += Time.deltaTime;
+            float progress = Mathf.Clamp01(elapsedTime / duration);
+
+            sun.intensity = Mathf.Lerp(initialIntensity, targetIntensity, progress);
+            sun.transform.eulerAngles = Vector3.Lerp(initialRotation, targetRotation, progress);
+
+            yield return null;
+        }
+
+        RenderSettings.skybox = targetSkybox;
+        onComplete?.Invoke();
+        isTransitioning = false;
+    }
 }
